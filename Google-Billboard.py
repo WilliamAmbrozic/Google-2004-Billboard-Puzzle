ba5b488a2c127d5000e9e0840f377a2e466befcf
William Ambrozic <william.ambrozic@gmail.com>
2018-10-05 12:29:07 -0400

    Update Google-Billboard.py

--- Google-Billboard.py
+++ Google-Billboard.py
@@ -8,7 +8,7 @@ def isPrime(n): #Simple Prime Checker O(sqrt(n)) Time Complexity
     and iterate whilst checking if there are any divisors other than 1. Normally you would 
     have some initial if statements like if (n == 1) return False; however since I only care
     about a 10 digit prime in consecutive digits of e there should never be a point were it 
-    calculates any n smaller than 10¹⁰. also since e starts with a 2, 1 wont even be a concern if 
+    calculates any n smaller than 10¹⁰. Also since e starts with a 2, 1 wont even be a concern if 
     someone wanted to calculate the first single digit prime. So do not be alarmed if this method 
     does not follow the stated convention.
     '''
@@ -34,15 +34,15 @@ def calc(c):
         try:
             if str(eMac)[n] == str(eBrother)[n] and n-20 > 0:
                 if len(e) >= c:
-                    e = e[1:] #removing first digit in search if we passed the desired length of prime
-                e += str(eMac)[n-20] #adding next digit of e
-        except IndexError: #an IndexError here implies the two digits are not equal becuase one or both are null
+                    e = e[1:] #Removing first digit in search if we passed the desired length of prime
+                e += str(eMac)[n-20] #Adding next digit of e
+        except IndexError: #An IndexError here implies the two digits are not equal becuase one or both are null
                            #I only care when they are numerically equal so any IndexError will be thrown
             continue
     return 0
 
 def main():
-    getcontext().prec = 150 #xxample precision for the digits of e (150 is all you need to get the answer)
+    getcontext().prec = 150 #Example precision for the digits of e (150 is all you need to get the answer)
     print(calc(10))
  
 main()
